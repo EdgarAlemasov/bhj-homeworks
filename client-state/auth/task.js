b62da1bcb5f2b5c655d6cd0f7157bc7e57c6089e
Edgar Alemasov <edgar.alemasov@gmail.com>
2022-11-07 20:34:10 +0500

    fixed

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -5,27 +5,32 @@ const input = document.querySelectorAll("input");
 const userId = document.getElementById("user_id");
 
 
-signUp.addEventListener("submit", (element) => {
-    const fornData = new FormData(signUp);
-    const request = new XMLHttpRequest();
+// window.localStorage.removeItem('id');
 
-    request.open("POST", "https://netology-slow-rest.herokuapp.com/auth.php");
-    request.addEventListener("readystatechange", function () {
-        if (this.readyState == request.DONE) {
-            const data = JSON.parse(this.responseText);
-            input.forEach(item => {
-                item.value ="";
-            });
-            if (data.success == true) {
+if (localStorage.getItem("id")) {
+    welcome.classList.add("welcome_active");
+    userId.textContent = `${localStorage.getItem("id")}`;
+} else {
+    signUp.addEventListener("submit", (event) => {
+        const formData = new FormData(signUp);
+        const request = new XMLHttpRequest();
+
+        request.open("POST", "https://netology-slow-rest.herokuapp.com/auth.php");
+        request.addEventListener("load", function () {
+            // console.log(this.response)
+            signUp.reset();
+            if (this.response.success == true) {
                 welcome.classList.add("welcome_active");
-                localStorage.setItem("id", data.user_id);
-                userId.textContent = `${data.user_id}`;
+                localStorage.setItem("id", `${this.response.user_id}`);
+                userId.textContent = `${this.response.user_id}`;
             } else {
                 alert("Неверный логин или пароль");
-            }
-        }
-    });
-    request.send(fornData);
-    element.preventDefault();
-})
+            };
+        });
+        request.responseType = "json";
+        request.send(formData);
+        event.preventDefault();
+    })
+}
+
 
